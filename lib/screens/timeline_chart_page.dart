a44cc35bfb8423dbaaa63410a8715c3b0eeb9872
hrajwade96 <hrishikeshrajwade96@gmail.com>
2024-07-03 16:46:44 +0530

    added guidelines

--- lib/screens/timeline_chart_page.dart
+++ lib/screens/timeline_chart_page.dart
@@ -2,16 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
 import '../timeline.dart';
-import '../timeline_chart.dart';
+import '../timeline_viewer.dart';
 
-class TimelineChartPage extends StatefulWidget {
-  const TimelineChartPage({super.key});
+class TimelineViewerPage extends StatefulWidget {
+  const TimelineViewerPage({super.key});
 
   @override
-  TimelineChartPageState createState() => TimelineChartPageState();
+  TimelineViewerPageState createState() => TimelineViewerPageState();
 }
 
-class TimelineChartPageState extends State<TimelineChartPage> {
+class TimelineViewerPageState extends State<TimelineViewerPage> {
+  static const String guidelinesText = '''
+Guidelines:
+1. Click on 'Import JSON' to upload your timeline events.
+2. The JSON file should be an array of events.
+3. Each event should include the following fields:
+   - id (int): A unique identifier for the event.
+   - content (string): A description of the event.
+   - start (int): The start time of the event in milliseconds since epoch.
+   - end (int): The end time of the event in milliseconds since epoch.
+
+Sample JSON:
+[
+  {"id": 1, "content": "Event 1", "start": 1622559600000, "end": 1622563200000},
+  {"id": 2, "content": "Event 2", "start": 1622563200000, "end": 1622566800000}
+]
+''';
+
   List<TimelineEvent> events = [];
   bool _isFullScreen = false;
 
@@ -56,7 +73,7 @@ class TimelineChartPageState extends State<TimelineChartPage> {
       appBar: _isFullScreen
           ? null
           : AppBar(
-              title: const Text('Timeline Visualizer'),
+              title: const Text('Timeline Viewer'),
               actions: [
                 IconButton(
                   icon: const Icon(Icons.fullscreen),
@@ -74,12 +91,27 @@ class TimelineChartPageState extends State<TimelineChartPage> {
                 child: const Text('Import JSON'),
               ),
             ),
+          if (events.isEmpty)
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Card(
+                elevation: 4.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: SelectableText(
+                    guidelinesText,
+                    style: TextStyle(fontSize: 16, height: 1.5),
+                  ),
+                ),
+              ),
+            ),
           Expanded(
             child: events.isEmpty
                 ? const Center(
                     child: Text(
                         "Please select a JSON file to display the timeline."))
-                : TimelineChart(
+                : TimelineViewer(
                     events: events,
                     isFullScreen: _isFullScreen,
                     toggleFullScreen: toggleFullScreen),
---
+++ lib/timeline_viewer.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:timeline_creator/timeline.dart';
+
+class TimelineViewer extends StatefulWidget {
+  final List<TimelineEvent> events;
+  final bool isFullScreen;
+  final VoidCallback toggleFullScreen;
+
+  const TimelineViewer(
+      {super.key,
+      required this.events,
+      required this.isFullScreen,
+      required this.toggleFullScreen});
+
+  @override
+  TimelineViewerState createState() => TimelineViewerState();
+}
+
+class TimelineViewerState extends State<TimelineViewer> {
+  double _zoomLevel = 50.0; // More zoomed out by default
+  List<TimelineEvent> _filteredEvents = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredEvents = widget.events;
+  }
+
+  void _filterEvents(String query) {
+    setState(() {
+      _filteredEvents = widget.events
+          .where((event) =>
+              event.content.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int earliestStartTime = _filteredEvents.isNotEmpty
+        ? _filteredEvents
+            .map((event) => event.start)
+            .reduce((a, b) => a < b ? a : b)
+        : 0;
+
+    return Column(
+      children: [
+        if (!widget.isFullScreen)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                labelText: 'Search Events',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: _filterEvents,
+            ),
+          ),
+        Expanded(
+          child: _filteredEvents.isEmpty
+              ? const Center(child: Text("No matching events found."))
+              : SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                                width: 150), // Space for context text
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: _filteredEvents.map((event) {
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 4.0),
+                                  child: Text(event.content),
+                                );
+                              }).toList(),
+                            ),
+                            const SizedBox(width: 20),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: _filteredEvents.map((event) {
+                                final duration = event.end - event.start;
+                                final offset = event.start - earliestStartTime;
+                                final color = _getColorForDuration(duration);
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 4.0),
+                                  child: Row(
+                                    children: [
+                                      SizedBox(width: offset / _zoomLevel),
+                                      Tooltip(
+                                        message:
+                                            "${event.content}\nStart: ${_formatTimestamp(event.start)}\nEnd: ${_formatTimestamp(event.end)}\nDuration: $duration ms",
+                                        child: GestureDetector(
+                                          onTap: () => _showEventDetails(event),
+                                          child: Container(
+                                            width: duration / _zoomLevel,
+                                            height: 20,
+                                            color: color,
+                                            child: Center(
+                                              child: Text(
+                                                "$duration ms",
+                                                style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.zoom_out),
+              onPressed: () {
+                setState(() {
+                  _zoomLevel = (_zoomLevel * 1.2).clamp(1.0, 1000.0);
+                });
+              },
+            ),
+            Text('Zoom: ${100 - _zoomLevel.floor()}'),
+            IconButton(
+              icon: const Icon(Icons.zoom_in),
+              onPressed: () {
+                setState(() {
+                  _zoomLevel = (_zoomLevel / 1.2).clamp(1.0, 1000.0);
+                });
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {
+                setState(() {
+                  _zoomLevel = 50.0;
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(widget.isFullScreen
+                  ? Icons.fullscreen_exit
+                  : Icons.fullscreen),
+              onPressed: widget.toggleFullScreen,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  void _showEventDetails(TimelineEvent event) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(event.content),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Start: ${_formatTimestamp(event.start)}"),
+              Text("End: ${_formatTimestamp(event.end)}"),
+              Text("Duration: ${event.end - event.start} ms"),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  String _formatTimestamp(int timestamp) {
+    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    return "${dateTime.minute}:${dateTime.second}.${dateTime.millisecond}";
+  }
+
+  Color _getColorForDuration(int duration) {
+    if (duration < 500) {
+      return Colors.lightBlue[200]!;
+    } else if (duration < 1000) {
+      return Colors.lightBlue[400]!;
+    } else if (duration < 1500) {
+      return Colors.blue[600]!;
+    } else {
+      return Colors.blue[800]!;
+    }
+  }
+}
